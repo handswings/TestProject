a6dc34ee2518ca1e8f61f52e869d7850efebedc2
Маруся <@Levsky.Dlink>
2018-03-22 21:42:50 +0300

    new commit

---
+++ TestProject/src/PrintAnynthingPreset.java
@@ -0,0 +1,12 @@
+
+public class PrintAnynthingPreset {
+
+	public static void printIt() {
+		System.out.println("Olka");
+	}
+	
+	public static void printItAgain() {
+		System.out.println("OlkaOlkaOlkaOlkaOlkaOlka");
+	}
+	
+}
