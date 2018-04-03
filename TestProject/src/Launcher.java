be0d075f483e72f76bd9e033355f227ec132359e
Маруся <@Levsky.Dlink>
2018-04-03 20:19:25 +0300

    Измененный главный метод

--- TestProject/src/Launcher.java
+++ TestProject/src/Launcher.java
@@ -3,6 +3,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		PrintAnynthingPreset.printIt();
-		PrintAnynthingPreset.printIt();
+		PrintAnynthingPreset.print();
+		MathRadom.haha();
 	}
 }
